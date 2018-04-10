2b61844e41f4c81ad69e4603cf74f640202055b8
成 <cheng@chengdeMacBook-Pro.local>
2018-04-10 13:29:22 +0800

    1002

--- 1002/main.cpp
+++ 1002/main.cpp
@@ -34,8 +34,55 @@
 
 #include <stdio.h>
 #include <string.h>
+
 int main() {
+    int n, i, j, k, l;
+    scanf("%d", &n);
+    char a[1001] = {0}, b[1001] = {0};
+    int la, lb, len;
+    for (i = 1; i <= n; i++) {
+        len = 0;
+        int c[1001] = {0};
+        scanf("%s %s", a, b);
+        printf("Case %d:\n%s + %s = ",i,a,b);
+        la = strlen(a);
+        lb = strlen(b);
+        
+        for (j = la - 1, k = lb - 1; j >= 0 && k >= 0; --j, --k) {
+            c[len] += a[j] - '0' + b[k] - '0';
+            if (c[len] > 9) {
+                c[len] -= 10;
+                c[len + 1]++;
+            }
+            len++;
+        }
+
+        if (j > 0) {
+            for ( ; j >= 0; --j) {
+                c[len++] += a[j] - '0';
+            }
+        } else if (k > 0) {
+            for ( ; k >= 0; --k) {
+                c[len++] += b[k] - '0';
+            }
+        }
+        
+        if (c[len]) {
+            len++;
+        }
+
+        for (l = len - 1; l >= 0; --l) {
+            printf("%d", c[l]);
+        }
+
+        printf("\n");
+        if (i < n) {
+            printf("\n");
+        }
+    }
+
     return 0;
 }
+
 
 
